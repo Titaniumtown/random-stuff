8544420bbd12d1db2866428543e6061b5a5ec9b0
Simon Gardling <titaniumtown@gmail.com>
2020-10-12 13:14:20 -0400

    update scripts and configs

--- kernel-build/bak_config.sh
+++ kernel-build/bak_config.sh
@@ -3,6 +3,7 @@
 bak_dir="/usr/src/build"
 
 lqx_dir="/usr/src/linux-5.8.14-liquorix"
+linux_clang_lto_dir="/usr/src/linux-clang-lto"
 
 echo "backing up lqx config..."
 if [ -d ${lqx_dir} ]; then
@@ -15,3 +16,13 @@ else
    echo "lqx dir not found, skipping..."
 fi
 
+echo "backing up linux-clang-lto config..."
+if [ -d ${linux_clang_lto_dir} ]; then
+   if [ -f "${linux_clang_lto_dir}/.config" ]; then
+      cat ${linux_clang_lto_dir}/.config > ${bak_dir}/config_linux_clang_lto
+   else
+      echo "linux-clang-lto config not found, skipping..."
+   fi
+else
+   echo "linux-clang-lto dir not found, skipping..."
+fi
