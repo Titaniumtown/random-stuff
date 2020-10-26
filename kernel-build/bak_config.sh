1341fca831123b4c5115c5c8897334e1da47578d
Simon Gardling <titaniumtown@gmail.com>
2020-10-26 10:50:40 -0400

    update scripts and configs

--- kernel-build/bak_config.sh
+++ kernel-build/bak_config.sh
@@ -4,6 +4,7 @@ bak_dir="/usr/src/build"
 
 lqx_dir="/usr/src/linux-5.8.16-pf1"
 pf_dir="/usr/src/linux-5.9_p1-pf"
+ck_dir="/usr/src/linux-5.9.1-ck"
 
 echo "backing up lqx config..."
 if [ -d ${lqx_dir} ]; then
@@ -17,7 +18,7 @@ else
 fi
 
 echo "backing up pf config..."
-if [ -d ${pf_dir_dir} ]; then
+if [ -d ${pf_dir} ]; then
    if [ -f "${pf_dir}/.config" ]; then
       cat ${pf_dir}/.config > ${bak_dir}/config_pf
    else
@@ -25,4 +26,16 @@ if [ -d ${pf_dir_dir} ]; then
    fi
 else
    echo "pf dir not found, skipping..."
+fi
+
+
+echo "backing up ck config..."
+if [ -d ${ck_dir} ]; then
+   if [ -f "${ck_dir}/.config" ]; then
+      cat ${ck_dir}/.config > ${bak_dir}/config_ck
+   else
+      echo "ck config not found, skipping..."
+   fi
+else
+   echo "ck dir not found, skipping..."
 fi
